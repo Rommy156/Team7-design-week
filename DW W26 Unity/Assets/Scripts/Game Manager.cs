15aad62267191011fa53012f3667572547337eb7
Evan-palmtree314 <evan.arsenault@mohawkcollege.ca>
2026-02-13 03:01:23 -0500

    added timer ui
    still need to add the scenes into the game over part of this and rounds aren't done yet just need to grab the stuff from main

--- DW W26 Unity/Assets/Scripts/Game Manager.cs
+++ DW W26 Unity/Assets/Scripts/Game Manager.cs
@@ -11,17 +11,33 @@ public class GameManager : MonoBehaviour
     // 3. have a way to check if the 6 players have gotten to a win state or after the tijers up decide a winner
 
 
+    // to do thrusday//
+    // 1. connect any team and status based info from player controller and hp scripts and port them here
+    // 2. have it so when a round is done it starts a new round 
+    // 3. have a way to declare a final winner (MAKE THIS PUBLIC SO OTHER SCRIPTS CAN USE) 
+    
+
     [SerializeField] private GameObject roundSystem = null;
     [SerializeField] public int scoreT1;
     [SerializeField] public int scoreT2;
     [SerializeField] public int WinScore;
+    public PlayerController playerController;
     Timer a;
-    [SerializeField] string TimerUiAAAA;
+    bool win;
+     string TimerUiAAAA;
+    private bool roundWinCheck;
     public void Awake()
     {
         round1Fight(scoreT1, scoreT2);
         Timeroutput(TimerUiAAAA, a);
+
         
+        win = CheckForWin(scoreT1,scoreT2,WinScore);
+        if (win = true)
+        {
+            Debug.Log("GAME OVER");
+        }
+       
     }
 
     public static void round1Fight(int a, int b)
@@ -29,17 +45,20 @@ public class GameManager : MonoBehaviour
         a = 0;
         b = 0;
 
+        
+
         return;
 
     }
-    public string Timeroutput(string output, Timer DaTime)
+    public string Timeroutput(string output, Timer DaTime )
     {
         //score += points;
-        DaTime.TIMEPRIME(output);
+        DaTime.TIMEPRIME(output); // problem here with string output
         return output;
     }
     public bool CheckForWin(int a, int b, int c)
     {
+        
         if (a == c)
         {
             Debug.Log("yooo team 1 wins");
@@ -50,9 +69,14 @@ public class GameManager : MonoBehaviour
             Debug.Log("team 2 wins");
             return false;
         }
-        else
-        {
-            return false;
-        }
+       return false;
+        
+        
+    }
+    
+    public int AddScoreToTeam(int a)
+    {
+        a++;
+        return a;
     }
 }
--- DW W26 Unity/Assets/Scripts/Timer.cs
+++ DW W26 Unity/Assets/Scripts/Timer.cs
@@ -10,12 +10,26 @@ public class Timer : MonoBehaviour
     // Update is called once per frame
     public void Update()
     {
+        if (RoundTimer > 0)
+        {
+            RoundTimer -= Time.deltaTime;
+
+        }
+        else if (RoundTimer < 0)
+        {
+            RoundTimer = 0;
+            // add game over here when done
+            RoundTimerText.color = Color.red;
+
+        }
+
         RoundTimer -= Time.deltaTime;
         int mins = Mathf.FloorToInt(RoundTimer/60);
         int secs = Mathf.FloorToInt(RoundTimer % 60);
-        RoundTimerText.text = string.Format("{0.00}:{1:00}",mins,secs);
+        RoundTimerText.text = string.Format("{0:00}:{1:00}", mins, secs);
 
-       
+
+
     }
     public string TIMEPRIME(string output)
     {
